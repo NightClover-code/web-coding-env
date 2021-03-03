271b3bed1610a3b83d6d6527980bcd781639f942
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-03-03 21:48:52 +0100

    added attribution

--- packages/local-client/src/components/App.tsx
+++ packages/local-client/src/components/App.tsx
@@ -8,6 +8,21 @@ const App: React.FC = () => {
   return (
     <div>
       <CellList />
+      <div
+        className="attribution"
+        style={{
+          textAlign: 'center',
+          opacity: '85%',
+          marginBottom: '20px',
+          letterSpacing: '2px',
+          fontStyle: 'light',
+        }}
+      >
+        <p>
+          Coded with 💖 by{' '}
+          <a href="https://github.com/NightClover-code">Achraf Elmouhib</a>
+        </p>
+      </div>
     </div>
   );
 };
